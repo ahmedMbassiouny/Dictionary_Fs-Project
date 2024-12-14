a82fdedc8f3871ed2a85ed2e951ad5f00093081f
Omar Ayman <96844139+maro222@users.noreply.github.com>
2024-12-14 05:09:56 +0200

    Update Program.fs
    loading from file

--- Dictionary/Program.fs
+++ Dictionary/Program.fs
@@ -9,3 +9,11 @@ let mutable dictionary: Dictionary = Map.empty
 let saveToFile (filePath: string) =
     let jsonData = JsonSerializer.Serialize(dictionary)
     File.WriteAllText(filePath, jsonData)
+
+
+let loadFromFile (filePath: string) =
+    if File.Exists(filePath) then
+        let jsonData = File.ReadAllText(filePath)
+        dictionary <- JsonSerializer.Deserialize<Dictionary>(jsonData)
+    else
+        failwith "File not found!"
